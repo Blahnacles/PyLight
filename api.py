b32529f2f9d263ac5621da995311891529bce8ad
Simon Laffan <simon@laffan.dev>
2020-02-12 18:36:12 +1100

    api addition, with support for bulb properties as a json object via GET

---
+++ api.py
@@ -0,0 +1,32 @@
+# Bulb API backend
+"""
+Author: Simon Laffan
+Created: 12/02/2020
+Updated: 12/02/2020
+Version: 0.0.1
+https://github.com/Blahnacles
+"""
+# imports
+from flask import Flask, jsonify, request, escape
+import yeelight, json
+from yeelight import Bulb
+
+# setup
+# Hardcoding IP for now, as discover_bulbs() does not work
+bedroom_ip = "192.168.1.122"
+bedroom = Bulb(bedroom_ip)
+if bedroom.get_properties()['music_on']== '0':
+    try:
+        bedroom.start_music(port=5555)
+    except yeelight.BulbException:
+        pass
+
+
+app = Flask(__name__)
+@app.route("/api", methods=['GET'])
+def get_status():
+    return json.dumps(bedroom.get_properties())
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
